43b16dceaa551de00f95bb0a61259ea8ca34f988
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2021-05-29 20:11:18 -0500

    user and home controllers added, root to home#welcome added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'home#welcome'
+  get 'home/welcome'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  get '/lougout', to: 'sessions#destroy'  
+  get '/logout', to: 'sessions#destroy'  
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
