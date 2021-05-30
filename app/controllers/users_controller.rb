53c23c3e3460cdfa03340ec54b5d6774e2096509
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2021-05-29 23:20:12 -0500

    corrected file naming for convention, updated userscontroller, updated sign up form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        user = User.new(user_params)
+        if user.save
+            session[:user_id] = user.id
+            redirect_to logs_path
+        else
+            render :new
+        end
+    end
+
+end
