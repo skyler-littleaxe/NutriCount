047a6df6f52d10bc6a663adfd566202b34732fad
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2021-05-30 21:37:36 -0500

    added sessions controller w/ login and session create and destroy action. added current_user and logged_in? and current_user helper methods inside ApplicationHelper

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+    def new
+    end
+
+    def create
+        user = User.find_by(params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else
+            flash[:message] = "Invalid credentials. Please try again."
+            redirect_to '/login', alert: "Invalid credentials. Plase try again."
+        end
+    end
+
+
+    def destroy
+        session.delete(:user_id)
+        redirect_to root_path
+    end
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
         user = User.new(user_params)
         if user.save
             session[:user_id] = user.id
-            redirect_to logs_path
+            redirect_to user_path
         else
             render :new
         end
