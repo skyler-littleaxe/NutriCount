a07e903f642c1bf888e11e97aa9e81d8e594c2f3
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2021-05-30 20:32:07 -0500

    edited sign up form, defined user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,4 +14,10 @@ class UsersController < ApplicationController
         end
     end
 
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password, :password_confirmation, :first_name, :last_name, :bio, :weight, :height, :goal)
+    end
+
 end
