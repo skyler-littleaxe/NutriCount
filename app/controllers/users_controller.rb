8eac447ffd6316ca7ba92cfec1dd2dfc058df8c0
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2021-06-02 23:16:39 -0500

    added views for editing profile and credentials, showing profile, added corresponding  method actions to users_controller and custom routing in routes.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,12 +21,47 @@ class UsersController < ApplicationController
     end
 
     def show
+        @current_user
     end
+
+    def edit
+        if logged_in?
+            @current_user
+        else
+            redirect_to root_path
+        end
+    end
+
+    def update
+        @current_user.update(profile_update_params)
+    end
+
+    def edit_credentials
+        if logged_in?
+            @current_user
+        else
+            redirect_to root_path
+        end
+    end
+
+    def update_credentials
+        @current_user.update(credentials_update_params)
+    end
+
+
 
     private
 
     def user_params
         params.require(:user).permit(:username, :email, :password, :password_confirmation, :first_name, :last_name, :bio, :weight, :height, :goal)
+    end
+
+    def profile_update_params
+        params.require(:current_user).permit(:first_name, :last_name, :bio, :weight, :height, :goal)
+    end
+
+    def credentials_update_params
+        params.require(:current_user).permit(:username, :email, :password, :password_confirmation)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
-  get '/auth/:provider/callback', to: 'sessions#omniauth'  
+  get '/auth/:provider/callback', to: 'sessions#omniauth'
+  get '/users/:id/credentials/edit', to: 'users#edit_credentials', as: 'edit_credentials'
+  put '/users/:id/credentials', to: 'user#update_credentials'
+  patch '/users/:id/credentials', to: 'user#update_credentials'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
