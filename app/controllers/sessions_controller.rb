4b8c576fdccc638c9d3e32392e5db51a54a836b9
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2021-06-04 00:52:19 -0500

    debugged issues with params and redirection, added before_action for UsersController, fixed issues with params and views for user#edit and user#show

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     end
 
     def create
-        user = User.find_by(params[:username])
+        user = User.find_by(params[:email])
         if user && user.authenticate(params[:password])
             session[:user_id] = user.id
             redirect_to user_path(user)
@@ -21,7 +21,7 @@ class SessionsController < ApplicationController
         user = User.create_from_omniauth(auth)
         if user.valid?
             session[:user_id] = user.id
-            redirect_to user_path
+            redirect_to user_path(user)
         else
             flash[:message] = user.errors.full_messages.join(", ")
             redirect_to new_user_path
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    before_action :logged_in?, :redirect_if_not_logged_in, only: [:show, :edit, :update, :edit_credentials, :update_credentials]
 
     def new
         if !logged_in?
@@ -14,38 +15,37 @@ class UsersController < ApplicationController
         user = User.new(user_params)
         if user.save
             session[:user_id] = user.id
-            redirect_to user_path
+            redirect_to user_path(user)
         else
             render :new
         end
     end
 
     def show
-        @current_user
     end
 
     def edit
-        if logged_in?
-            @current_user
-        else
-            redirect_to root_path
-        end
+        
     end
 
     def update
-        @current_user.update(profile_update_params)
-    end
-
-    def edit_credentials
-        if logged_in?
-            @current_user
+        if @current_user.update(profile_update_params)
+            redirect_to user_path(@current_user)
         else
-            redirect_to root_path
+            render :edit
         end
     end
 
+    def edit_credentials
+        
+    end
+
     def update_credentials
-        @current_user.update(credentials_update_params)
+        if @current_user.update(credentials_update_params)
+            redirect_to user_path(@current_user)
+        else
+            render :edit
+        end
     end
 
 
@@ -53,15 +53,23 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:username, :email, :password, :password_confirmation, :first_name, :last_name, :bio, :weight, :height, :goal)
+        params.require(:user).permit(:username, :email, :password, :password_confirmation, :first_name, :last_name, :bio, :weight, :height, :goal, :lifestyle)
     end
 
     def profile_update_params
-        params.require(:current_user).permit(:first_name, :last_name, :bio, :weight, :height, :goal)
+        params.require(:user).permit(:first_name, :last_name, :bio, :weight, :height, :goal, :lifestyle)
     end
 
     def credentials_update_params
-        params.require(:current_user).permit(:username, :email, :password, :password_confirmation)
+        params.require(:user).permit(:username, :email, :password, :password_confirmation)
     end
 
+    def redirect_if_not_logged_in
+        if !logged_in?
+            redirect_to root_path
+        end
+    end
+
+
+    
 end
