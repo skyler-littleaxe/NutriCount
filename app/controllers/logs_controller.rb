60c1f64690d3f05c4097a90b2b3c6248b1e874c9
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2021-06-05 23:17:25 -0500

    added basic CRUD actions to log and meal controller, added missing attributes to users/new form

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,2 +1,39 @@
 class LogsController < ApplicationController
+    before_action :logged_in?, only: [:index, :show, :edit, :update,]
+    
+
+    def index
+        @logs = @current_user.log.all
+    end
+
+    def new
+        @log = Log.new
+        1.times { @log.meal.build}
+    end
+
+    def create
+        @log = Log.new(log_params)
+        if @brand.save
+            redirect_to brand_path(@brand)
+        else
+            while @log.meal.length < 1
+                @log.meal.build
+            end
+            render :new
+        end
+    end
+
+    def show
+        @log = Log.find(params[id])
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+end
 end
--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,2 +1,26 @@
 class MealsController < ApplicationController
+    before_action :logged_in?, only: [:index, :show, :edit, :update,]
+    
+
+    def index
+        
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
 end
