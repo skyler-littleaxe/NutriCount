d6aca7025ae630c05ae477d328b1088f47ee8e39
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2021-06-05 23:52:42 -0500

    completed CRUD controller logic for logs_controller, completed log_params and find_log before_action

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,5 +1,6 @@
 class LogsController < ApplicationController
-    before_action :logged_in?, only: [:index, :show, :edit, :update,]
+    before_action :logged_in?, only: [:index, :show, :edit, :update]
+    before_action :logged_in?, only: [:show, :edit, :update, :destroy]
     
 
     def index
@@ -24,16 +25,33 @@ class LogsController < ApplicationController
     end
 
     def show
-        @log = Log.find(params[id])
+        @log
     end
 
     def edit
+        @log
     end
 
     def update
+        if @log.update(log_params)
+            redirect_to log_path(@log)
+        else
+            render :edit
+        end
     end
 
     def destroy
+        @log.destroy
+        redirect_to logs_path
     end
-end
+    
+    private
+
+    def log_params
+        params.require(:log).permit(:date, :cheat_day, :cal_goal, :protein_goal, :carb_goal, :fat_goal, :water_goal, :sugar_goal, :cal_goal, :protein_count, :carb_count, :fat_count, :water_count, :sugar_count, :user_id)
+    end
+
+    def find_log
+        @log = Log.find(params[id])
+    end
 end
