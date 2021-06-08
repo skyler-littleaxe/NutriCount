1b93f5c4e7f85f86909a17848b1a0e205fd1e777
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2021-06-07 22:58:23 -0500

    debugging log_form partial

--- app/models/log.rb
+++ app/models/log.rb
@@ -6,6 +6,16 @@ class Log < ApplicationRecord
     validates :date, date: true
     validates :date, 
             date: { before_or_equal_to: Proc.new {Time.now} }
+    accepts_nested_attributes_for :meals
+
+    def meals_attributes=(meal_attributes)
+        meal_attributes.values.each do |meal_attribute|
+            if meal_attribute["meal_name"].present?
+                meal = Meal.find_or_create_by(meal_attribute)
+                self.meals << meal
+            end
+        end
+    end
     
 
 end
